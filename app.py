034b5c712fb799dd11b5d869f19269dfa5d04362
OkanShr <okan.shr@hotmail.com>
2024-08-20 00:11:14 +0200

    Improve file structure Add app.py Add moviwebapp new db for app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from datamanager.sqlite_data_manager import SQLiteDataManager
+
+app = Flask(__name__)
+data_manager = SQLiteDataManager('moviwebapp.db')
+
+
+@app.route('/')
+def home():
+    return "Welcome to MovieWeb App!"
+
+
+@app.route('/users')
+def list_users():
+    users = data_manager.get_all_users()
+    return str(users)  # Temporarily returning users as a string
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
